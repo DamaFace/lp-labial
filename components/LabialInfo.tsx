8855a3465074a58ed390d809f37993d46c96b374
DamaFace <marketingdamaface@gmail.com>
2025-08-15 17:31:42 +0000

    background: update image

--- components/LabialInfo.tsx
+++ components/LabialInfo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import background from 'public/back-full-face.jpeg';
+import background from 'public/back-labial.jpeg';
 
 const LabialInfo = () => {
   const areas = [
